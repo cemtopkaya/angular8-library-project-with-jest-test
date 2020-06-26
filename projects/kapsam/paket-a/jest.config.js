700682203e991c284388d71b20932892a85e76e9
Cem Topkaya <cem.topkaya@hotmail.com>
2020-06-26 03:18:49 +0300

    setupFilesAfterEnv ayari yapildi

--- projects/kapsam/paket-a/jest.config.js
+++ projects/kapsam/paket-a/jest.config.js
@@ -14,7 +14,39 @@ const setupFilesAfterEnv = [
 module.exports = {
   ...jestBaseConfig,
   rootDir: 'projects/kapsam/paket-a/src',
-  // setupFilesAfterEnv:['<rootDir>/../setupJest.ts'],
+  /**
+   * Her testten önce çalışmasını istediğimiz bir script varsa 
+   * setupFilesAfterEnv tam bu iş için biçilmiş kaftan.
+   * 
+   * Eğer ayarlar içinde yukarıdaki kodu açarsak, 
+    
+        setupFilesAfterEnv:['<rootDir>/setupJest.ts'],
+    
+   * aşağıdaki komut satırını konsolda çalıştırıp şu ayarları görebiliriz: 
+
+     $ ng test @kapsam/paket-a --showConfig | grep setupFilesAfterEnv -A3      
+      "setupFilesAfterEnv": [
+        "C:\\Users\\cem.topkaya\\git\\jest-ng-test\\node_modules\\@angular-builders\\jest\\dist\\jest-config\\setup.js",
+        "C:\\Users\\cem.topkaya\\git\\jest-ng-test\\projects\\kapsam\\paket-a\\src\\setupJest.ts"
+      ],
+   * 
+   * Yani başlangıçta çalışmasını istediğimiz dosyamız setupJest.ts dosyası listeye eklenmiş.
+   * Aynı dosyayı bu kez dizi değil ama string olacak şekilde angular.json içinde aşağıdaki gibi tanımlarsak:
+        "test": {
+          "builder": "@angular-builders/jest:run",
+          "options": {
+            "setupFilesAfterEnv":"<rootDir>/setupJest.ts",
+            "configPath":"./jest.config.js",
+            "tsConfig": "tsconfig.spec.json"
+          }
+        }
+   * 
+     $ ng test @kapsam/paket-a --showConfig | grep setupFilesAfterEnv -A3
+      "setupFilesAfterEnv": [
+        "C:\\Users\\cem.topkaya\\git\\jest-ng-test\\projects\\kapsam\\paket-a\\src\\setupJest.ts"
+      ],
+   */
+  // setupFilesAfterEnv:['<rootDir>/setupJest.ts'],
   //*************************************************** COVERAGE */
   collectCoverage: false,
   //***************************************************** REPORT */
